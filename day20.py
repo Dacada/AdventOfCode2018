5d81bb76491e80b99347b1326efdf274f53a14bb
donpolilla <davidcarreracasado@gmail.com>
2018-12-20 20:31:33 +0100

    Finished day 20 after encountring networkx

--- day20.py
+++ day20.py
@@ -1,4 +1,5 @@
 import util
+import networkx as nx
 
 DAY = 20
 
@@ -13,25 +14,26 @@ def invert(direction):
         return 'N'
 
 class Room:
-    def __init__(self):
+    def __init__(self, graph):
+        self.graph = graph
         self.neighbors = {}
+        graph.add_node(self)
 
     def add_neighbor(self, direction):
         if direction in self.neighbors:
             return self.neighbors[direction]
         else:
-            other = Room()
+            other = Room(self.graph)
             self.neighbors[direction] = other
             other.neighbors[invert(direction)] = self
+            self.graph.add_edge(self, other)
             return other
 
-def unique(l):
-    newl = []
-    for e in l:
-        if e not in newl:
-            newl.append(e)
-    l.clear()
-    l.extend(newl)
+    def eccentricity(self):
+        return nx.eccentricity(self.graph, self)
+
+    def shortest_paths_length(self, doors):
+        return sum(x >= 1000 for x in nx.shortest_path_length(self.graph, source=self).values())
 
 def up(c):
     return (c[0],c[1]-1)
@@ -80,42 +82,74 @@ def print_map(origin):
             if d in node.neighbors:
                 wallcoord = m[d](tophys(coord))
                 l[wallcoord[1]][wallcoord[0]] = '|' if d in 'WE' else '-'
-                nodes.append((node,tophys(m[d](coord))))
+                nodes.append((node.neighbors[d],m[d](coord)))
+
+    x,y = tophys(origincoords)
+    l[y][x] = 'X'
 
     for row in l:
         print(''.join(row))
-        
-@util.timing_wrapper
-def star1():
-    text = util.get_input_text(DAY)
-    text = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
-    text = "^ENWWW(NEEE|SSE(EE|N))$"
-    text = "^WNE$"
-    
+
+
+def make_graph(text):
+    """
     origin = Room()
     stack = []
     rooms = [origin] # all the "leaf" rooms we're following
-    
+
     for c in text:
         if c in 'NEWS':
             for i in range(len(rooms)):
                 rooms[i] = rooms[i].add_neighbor(c)
-            unique(rooms) # discard duplicates
+            rooms = unique(rooms) # discard duplicates
         elif c == '(':
-            stack.append((room,[])) # room from which we split and list of all the leaf rooms resulting
+            stack.append((unique(rooms),[])) # rooms from which we split and list of all the leaf rooms resulting
         elif c == '|':
-            stack[-1][1].append(room) # leaf room created
-            room = stack[-1][0] # back to the room to split from
+            stack[-1][1].extend(rooms) # leaf rooms created
+            stack[-1] = (stack[-1][0],unique(stack[-1][1]))
+            rooms = unique(stack[-1][0]) # back to the rooms we split from
         elif c == ')':
-            stack[-1][1].append(room) # last leaf
-            rooms.extend(stack[-1][1]) # add all leafs
-            unique(rooms) # discard duplicates
+            stack[-1][1].extend(rooms) # last leaf group
+            rooms.extend(stack[-1][1]) # add all leafs to our rooms
+            rooms = unique(rooms)
+    """
+    
+    #And then I checked Reddit. Oh joy, all groups end up in the same spot. I could've used the simple stack all this time.
+    #If you're reading this, Advent of Code guy, learn to write problem descriptions.
 
-    print_map(origin)
+    origin = Room(nx.Graph())
+    stack = []
+    room = origin
+
+    for c in text:
+        if c in 'NEWS':
+            room = room.add_neighbor(c)
+        elif c == '(':
+            stack.append(room)
+        elif c == '|':
+            room = stack[-1]
+        elif c == ')':
+            stack.pop()
+
+    return origin
+        
+@util.timing_wrapper
+def star1():
+    text = util.get_input_text(DAY)
+    #text = "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$" #31
+    #text = "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$" #23
+    #text = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$" #18
+    #text = "^ENWWW(NEEE|SSE(EE|N))$" #10
+    #text = "^WNE$" #3
+
+    origin = make_graph(text)
+
+    #print_map(origin)
+    return origin.eccentricity()
 
 @util.timing_wrapper
 def star2():
-    pass
+    return make_graph(util.get_input_text(DAY)).shortest_paths_length(1000)
 
 if __name__ == '__main__':
     util.pretty_print(star1, star2)
