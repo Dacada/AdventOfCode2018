5cb447f4c0d1f1f8db9f460bdc3e7987438bcfe9
donpolilla <davidcarreracasado@gmail.com>
2018-12-20 15:16:11 +0100

    But why

--- day20.py
+++ day20.py
@@ -12,7 +12,7 @@ def invert(direction):
     elif direction == 'S':
         return 'N'
 
-def Room:
+class Room:
     def __init__(self):
         self.neighbors = {}
 
@@ -25,24 +25,90 @@ def Room:
             other.neighbors[invert(direction)] = self
             return other
 
+def unique(l):
+    newl = []
+    for e in l:
+        if e not in newl:
+            newl.append(e)
+    l.clear()
+    l.extend(newl)
+
+def up(c):
+    return (c[0],c[1]-1)
+def down(c):
+    return (c[0],c[1]+1)
+def left(c):
+    return (c[0]-1,c[1])
+def right(c):
+    return (c[0]+1,c[1])
+
+def print_map(origin):
+    l = [['.']*100 for __ in range(100)]
+
+    for y in range(100):
+        for x in range(100):
+            if x % 2 != 0 or y % 2 != 0:
+                l[y][x] = '#'
+
+    nodes = [(origin,(50,50))]
+    seen = []
+    while nodes:
+        node,coord = nodes.pop()
+        
+        if node in seen:
+            continue
+        seen.append(node)
+        
+        if 'W' in node.neighbors:
+            wallcoord = right(coord)
+            l[wallcoord[1]][wallcoord[0]] = '|'
+            nodes.append((node,right(wallcoord)))
+        if 'E' in node.neighbors:
+            wallcoord = left(coord)
+            l[wallcoord[1]][wallcoord[0]] = '|'
+            nodes.append((node,left(wallcoord)))
+        if 'N' in node.neighbors:
+            wallcoord = up(coord)
+            l[wallcoord[1]][wallcoord[0]] = '-'
+            nodes.append((node,up(wallcoord)))
+        if 'S' in node.neighbors:
+            wallcoord = down(coord)
+            l[wallcoord[1]][wallcoord[0]] = '-'
+            nodes.append((node,down(wallcoord)))
+            
+    l[50][50] = 'X'
+
+    for row in l:
+        print(''.join(row))
+        
 @util.timing_wrapper
 def star1():
+    text = util.get_input_text(DAY)
+    text = "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$"
+    text = "^ENWWW(NEEE|SSE(EE|N))$"
+    text = "^WNE$"
+    
     origin = Room()
-
     stack = []
-    room = root
-    for c in util.get_input_text(DAY)[1:-1]:
+    rooms = [origin] # all the "leaf" rooms we're following
+    
+    for c in text:
         if c in 'NEWS':
-            room = room.add_neighbor(c)
+            for i in range(len(rooms)):
+                rooms[i] = rooms[i].add_neighbor(c)
+            unique(rooms) # discard duplicates
         elif c == '(':
-            stack.append((room,[]))
+            stack.append((room,[])) # room from which we split and list of all the leaf rooms resulting
         elif c == '|':
-            stack[-1][1].append(room)
-            room = stack[-1][0]
+            stack[-1][1].append(room) # leaf room created
+            room = stack[-1][0] # back to the room to split from
         elif c == ')':
-            stack[-1][1].append(room)
-            # now we have to treat all the rooms in stack[-1][1] as the "room"
-            # this calls for recursivity... ugh...
+            stack[-1][1].append(room) # last leaf
+            rooms.extend(stack[-1][1]) # add all leafs
+            unique(rooms) # discard duplicates
+
+    print(origin.neighbors['W'].neighbors)
+    #print_map(origin)
 
 @util.timing_wrapper
 def star2():
